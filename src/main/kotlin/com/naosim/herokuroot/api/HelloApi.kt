b005c6e235620a4383094db2e48aba007eac2336
naosim <nao_pillows@yahoo.co.jp>
2019-05-01 02:02:21 +0900

    helloworld api

---
+++ src/main/kotlin/com/naosim/herokuroot/api/HelloApi.kt
@@ -0,0 +1,19 @@
+package com.naosim.herokuroot.api
+
+import java.util.HashMap
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloApi {
+
+    @RequestMapping(value = ["/hello"], method = [RequestMethod.GET])
+    operator fun invoke(): Map<*, *> {
+        val res = HashMap<String, Any>()
+        res["hello"] = "hello"
+        return res
+    }
+
+
+}
